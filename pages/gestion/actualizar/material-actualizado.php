91f78b8aedb84f8a90b82715f8dc0522c2d73b58
Rodrigo <rodrigo.gamero.luque@gmail.com>
2022-02-01 18:47:03 +0100

    Arreglo actualizar material y actualiza bien lets goo

---
+++ pages/gestion/actualizar/material-actualizado.php
@@ -0,0 +1,28 @@
+<?php
+include '../../../datosBBDD.php';
+
+    error_reporting(0);
+    $nserie = $_POST['nserie'];
+    echo $nserie;
+    $nombre_material = strtolower($_POST['nombre_material']);
+    echo $nombre_material;
+    $marca = $_POST['marca'];
+    echo $marca;
+    $modelo = $_POST['modelo'];
+    echo $modelo;
+    $estado = strtolower($_POST['estado']);
+    echo $estado;
+    $observaciones = $_POST['observaciones'];
+    echo $observaciones;
+
+    $update = "UPDATE materiales SET nombre_materiales = '$nombre_material', marca = '$marca',
+     modelo = '$modelo', estado = '$estado', observaciones = '$observaciones' WHERE num_serie = '$nserie'";
+    $conexion = mysqli_connect($servername, $username, $password, $database);
+
+    if (mysqli_query($conexion, $update)) {
+        echo "<script>alert('El material ha sido actualizado.')</script>";
+        header('Location: actualizar-materiales.php?varId2=' .  $_POST['nserie']);
+    } else {
+        echo "<div class='alert alert-danger' role='alert' style='width: 70%;margin: auto;margin-top: 2rem;text-align: center;'>El material no se actualizó por un error inesperado.</div>";
+    }
+ ?>
